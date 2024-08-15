76827443714df53314f48db70b70d0e832dea735
dev-igss <danielvel1977@gmail.com>
2024-08-15 10:27:44 -0600

    Update pdf.blade.php

--- code/code-dev/resources/views/admin/solicitudes/boletas_despacho/pdf.blade.php
+++ code/code-dev/resources/views/admin/solicitudes/boletas_despacho/pdf.blade.php
@@ -76,15 +76,15 @@
         </div>
 
         <div style="position: relative; top: 46.5mm; left: 43.3mm;  width: 175px; height: 10px; font-size: 12px;"  >
-            {{ $d->ruta_asignada->ruta_solicitud->nombre_piloto}}           
+            {{ $d->ruta_asignada}}           
         </div>
 
         <div style="position: relative; top: 36.4mm; left: 96mm;  width: 100px; height: 10px; font-size: 12px;"  >
-            {{ $d->ruta_asignada->ruta_solicitud->placa_vehiculo}}           
+            {{ $d->ruta_asignada}}           
         </div>
 
         <div style="position: relative; top: 40.6mm; left: 99mm;  width: 100px; height: 10px; font-size: 12px;"  >
-            {{ $d->ruta_asignada->ruta_solicitud->no_licencia}}           
+            {{ $d->ruta_asignada}}           
         </div>
 
         @foreach($d->detalles as $det)
