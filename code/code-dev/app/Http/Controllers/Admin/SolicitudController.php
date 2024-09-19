0b7551563069a491be7a315e800fadfbc6d5b180
dvlsqz <danielvel1977@gmail.com>
2024-09-18 20:33:58 -0600

    Update SolicitudController.php

--- code/code-dev/app/Http/Controllers/Admin/SolicitudController.php
+++ code/code-dev/app/Http/Controllers/Admin/SolicitudController.php
@@ -1396,7 +1396,7 @@ class SolicitudController extends Controller
                 DB::raw('solicitud_detalles.id_escuela as escuela_id'),
                 DB::raw('SUM( solicitud_detalles.dias_de_solicitud) as dias'),
                 DB::raw('SUM(Distinct solicitud_detalles.total_pre_primaria_a_tercero_primaria) as total_beneficiarios'),
-                DB::raw('raciones.id as idracion')
+                DB::raw('raciones.id as idracion'),
                 DB::raw('raciones.nombre as racion'),
             )
             ->join('raciones', 'raciones.id', 'solicitud_detalles.tipo_de_actividad_alimentos')
