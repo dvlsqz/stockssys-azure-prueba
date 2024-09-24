45dd8defe94149d2b896f121b20f56194199ff87
dev-igss <danielvel1977@gmail.com>
2024-09-24 15:09:24 -0600

    Update SolicitudController.php

--- code/code-dev/app/Http/Controllers/Admin/SolicitudController.php
+++ code/code-dev/app/Http/Controllers/Admin/SolicitudController.php
@@ -1474,8 +1474,8 @@ class SolicitudController extends Controller
                 DB::raw('alimentos_racion.cantidad as alimento_peso'),
                 DB::raw('solicitud_detalles.tipo_de_actividad_alimentos as racion'),
             )
-            ->join(DB::RAW("(SELECT id_racion, id_alimento, cantidad FROM alimentos_raciones GROUP BY id_racion, id_alimento, cantidad) as alimentos_racion"), function($j){
-                $j->on("alimentos_racion.id_racion","=","solicitud_detalles.tipo_de_actividad_alimentos");
+            ->join(DB::RAW("(SELECT id_racion, id_alimento, cantidad FROM alimentos_raciones GROUP BY id_racion, id_alimento, cantidad) as alimentos_racion"), function($j) use($id_escolar_racion){
+                $j->where("alimentos_racion.id_racion","=",$id_escolar_racion);
             })
             ->join('bodegas', 'bodegas.id', 'alimentos_racion.id_alimento')
             ->where('solicitud_detalles.id_solicitud', $solicitud)  
