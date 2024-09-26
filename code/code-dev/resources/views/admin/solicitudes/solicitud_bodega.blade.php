e9733f20f6293f2ddb7fd3620c7a1445dc2a8fdc
dev-igss <danielvel1977@gmail.com>
2024-09-26 09:37:25 -0600

    Update solicitud_bodega.blade.php

--- code/code-dev/resources/views/admin/solicitudes/solicitud_bodega.blade.php
+++ code/code-dev/resources/views/admin/solicitudes/solicitud_bodega.blade.php
@@ -31,6 +31,7 @@ ini_set('max_execution_time', 0); ?>
                                             Dias: <b>{{ $det_preprimaria_enc->dias}} </b>&nbsp
                                             Total Beneficiarios:  <b>{{ $det_preprimaria_enc->total_beneficiarios}}  </b>
                                             @php($tipo_racion_escolar = $det_preprimaria_enc->idracion)
+                                            {{$tipo_racion_escolar.'-'.$det_preprimaria_enc->idracion  }}
                                         @endif                                   
                                     @endforeach 
                                     <br>
