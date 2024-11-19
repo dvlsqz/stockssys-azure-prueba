447a3475828ca6189439f10d65a11564ab73431a
dev-igss <danielvel1977@gmail.com>
2024-11-19 09:28:48 -0600

    Update solicitud_insumos_pdf.blade.php

--- code/code-dev/resources/views/admin/bodega/bodega_socio/solicitud_insumos_pdf.blade.php
+++ code/code-dev/resources/views/admin/bodega/bodega_socio/solicitud_insumos_pdf.blade.php
@@ -81,10 +81,10 @@
                             <td> {{ $a->cantidad }} </td> 
                         @endif                                                            
                     @endforeach  
-                    <td>{{$det->alimento_bodega_socio->pesos_alimento}}</td>
-                    <!--@foreach($det->alimento_bodega_socio->pesos_alimento as $p)                                                                               
-                        <td> {{ ceil($p->kg_x_unidad) }} </td> 
-                    @endforeach   -->
+                    <!--<td>{{$det->alimento_bodega_socio->pesos_alimento}}</td>-->
+                    @foreach($det->alimento_bodega_socio->pesos_alimento as $p)                                                                               
+                        <td> {{$p->gramos_x_kg }} </td> 
+                    @endforeach   
                     <td>{{$det->no_unidades}}</td>
                     <td>{{$det->alimento_bodega_socio->saldo}}</td>
                     <td>{{$det->alimento_bodega_socio->saldo}}</td>
