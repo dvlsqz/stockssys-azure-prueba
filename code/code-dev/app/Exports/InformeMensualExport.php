b60812b8988b2143ee74e868a8f73e28b0ad7f99
dev-igss <danielvel1977@gmail.com>
2024-11-25 12:52:31 -0600

    Update InformeMensualExport.php

--- code/code-dev/app/Exports/InformeMensualExport.php
+++ code/code-dev/app/Exports/InformeMensualExport.php
@@ -399,11 +399,11 @@ class InformeMensualExport implements FromView, WithEvents, WithDrawings, WithTi
                         }
 
                         if($alimentos[$d]->nombre == 'Aceite'){
-                            $event->sheet->setCellValue($prueba[$i].'20', '=('.$prueba[$i].'19*18.4615)/1000')->setFormatCode('0.00');
+                            $event->sheet->setCellValue($prueba[$i].'20', '=ROUND(('.$prueba[$i].'19*18.4615)/1000,2)');
                         }elseif($alimentos[$d]->nombre == 'Maíz BIO'){
-                            $event->sheet->setCellValue($prueba[$i].'20', '=('.$prueba[$i].'19*45.3592)/1000')->setFormatCode('0.00');
+                            $event->sheet->setCellValue($prueba[$i].'20', '=ROUND(('.$prueba[$i].'19*45.3592)/1000,2)');
                         }else{
-                            $event->sheet->setCellValue($prueba[$i].'20', '=('.$prueba[$i].'19*50)/1000')->setFormatCode('0.00');
+                            $event->sheet->setCellValue($prueba[$i].'20', '=ROUND(('.$prueba[$i].'19*50)/1000,2)');
                         }
 
                         $event->sheet->setCellValue($prueba[$i].'9', $alimentos[$d]->nombre);
