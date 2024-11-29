5205f191c3e643f2ff014a73f127af9ee74adf89
dev-igss <danielvel1977@gmail.com>
2024-11-29 15:11:15 -0600

    Update boleta_ruta_confirmada_pdf.blade.php

--- code/code-dev/resources/views/admin/solicitudes/boleta_ruta_confirmada_pdf.blade.php
+++ code/code-dev/resources/views/admin/solicitudes/boleta_ruta_confirmada_pdf.blade.php
@@ -269,7 +269,7 @@
         </table>
     </div>
 
-    <div style="page-break-before:auto; display: inline-block; top: 1000px;">
+    <div style="page-break-before:auto; display: flex;">
         <div style="text-align: left; width: 50%; display: inline-block; float:left;">
             <b>Entregado conforme<br>
             <b>Bodega PSC Salcaja<br>
